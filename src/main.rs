e15638eb08c6e4ece6f66984de1bf429cba6b359
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2023-09-10 20:31:35 -0400

    [src/main.rs] Start implementing CLI with `ls` and config read/write ; [Cargo{.toml,.lock}] Use any patch version of deps ; add new dep of serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,30 @@
-// #[macro_use]
 extern crate clap_markdown;
 extern crate version_manager_rs;
 
-version_manager_rs::cli_struct!(
+version_manager_rs::cli_struct_and_helpers!(
     env!("CARGO_PKG_NAME"),
     env!("CARGO_PKG_AUTHORS"),
     env!("CARGO_PKG_VERSION"),
     env!("CARGO_PKG_DESCRIPTION")
 );
 
-fn main() {
-    let args = Cli::parse();
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = {
+        let mut _args = Cli::parse();
+        config_from_file(&mut _args)?.unwrap_or_else(|| _args)
+    };
     if args.markdown_help {
         clap_markdown::print_help_markdown::<Cli>();
-        return;
-    } else if !args.vms_config.is_empty() && std::path::Path::new( &args.vms_config).components().next() == Some(std::path::Component::Normal(std::ffi::OsStr::new("$HOME"))) {
-        println!("got a config file {:?}", args.vms_config)
+        return Ok(());
     }
     match &args.command {
         Commands::Ls {} => {
-            unimplemented!("TODO")
-        },
+            default_ls_command(&args)?;
+        }
         _ => panic!("No command given"),
     }
+    if should_write_to_config(&args) {
+        config_file_write(&args)?;
+    }
+    Ok(())
 }
