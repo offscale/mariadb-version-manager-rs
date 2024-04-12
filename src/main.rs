3d80db27376efb273a9ea698c8da39696ff7a7c3
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-04-12 09:26:31 -0400

    [src/{main,mariadb_vm}.rs] Move logic around

--- src/main.rs
+++ src/main.rs
@@ -10,105 +10,6 @@ version_manager_rs::cli_struct_and_helpers!(
     3306_u16 // default port
 );
 
-/*
-# To support old versions for testing purposes which are not on dlm.mariadb.com
-case ${mariadb_server_version} in
-    *10.1[0-9]*) ;; # everything >= 10.10 is on the new dlm for sure
-    *10.0*) ;& # 10.0 releases are on the old download server only
-    *10.1*) ;& # same for 10.1
-    *10.2*) ;& # same for 10.2
-    *10.3.[0-2]*|*10.3.[0-9]) ;& # 10.3: old server has <= .32, dlm has >= .29, we switch at .30
-    *10.4.[0-1]*|*10.4.[0-9]) ;& # 10.4: dlm has >= 10.4.20
-    *10.5.[0-9])     # 10.5: dlm has >= 10.5.10
-    url_base="downloads.mariadb.com"
-        url_mariadb_repo="https://${url_base}/MariaDB"
-    mariadb_server_version_real=$mariadb_server_version
-    ;;
-esac
-*/
-
-const AOT_MARIADB_VERSIONS_LEN: usize = 10;
-
-fn parse_from_year_month_date(date_str: &str) -> chrono::ParseResult<chrono::NaiveDate> {
-    chrono::NaiveDate::parse_from_str(date_str, "%Y-%m-%d")
-}
-
-// From https://dlm.mariadb.com/rest/releases/mariadb_server/ 2024-04-06
-fn offline_major_releases() -> Vec<mariadb_vm::MajorReleases> {
-    let mut releases = std::vec::Vec::with_capacity(AOT_MARIADB_VERSIONS_LEN);
-    releases[0] = mariadb_vm::MajorReleases {
-        release_id: String::from("11.5"),
-        release_name: String::from("MariaDB Server 11.5"),
-        release_status: String::from("Alpha"),
-        release_support_type: String::from("Short Term Support"),
-        release_eol_date: None,
-    };
-    releases[1] = mariadb_vm::MajorReleases {
-        release_id: String::from("11.4"),
-        release_name: String::from("MariaDB Server 11.4"),
-        release_status: String::from("RC"),
-        release_support_type: String::from("Long Term Support"),
-        release_eol_date: None,
-    };
-    releases[2] = mariadb_vm::MajorReleases {
-        release_id: String::from("11.3"),
-        release_name: String::from("MariaDB Server 11.3"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Short Term Support"),
-        release_eol_date: None,
-    };
-    releases[3] = mariadb_vm::MajorReleases {
-        release_id: String::from("11.2"),
-        release_name: String::from("MariaDB Server 11.2"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Short Term Support"),
-        release_eol_date: Some(parse_from_year_month_date("2024-11-21").unwrap()),
-    };
-    releases[4] = mariadb_vm::MajorReleases {
-        release_id: String::from("11.1"),
-        release_name: String::from("MariaDB Server 11.1"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Short Term Support"),
-        release_eol_date: Some(parse_from_year_month_date("2024-08-21").unwrap()),
-    };
-    releases[5] = mariadb_vm::MajorReleases {
-        release_id: String::from("11.0"),
-        release_name: String::from("MariaDB Server 11.0"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Short Term Support"),
-        release_eol_date: Some(parse_from_year_month_date("2024-06-06").unwrap()),
-    };
-    releases[6] = mariadb_vm::MajorReleases {
-        release_id: String::from("10.11"),
-        release_name: String::from("MariaDB Server 10.11"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Long Term Support"),
-        release_eol_date: Some(parse_from_year_month_date("2028-02-16").unwrap()),
-    };
-    releases[7] = mariadb_vm::MajorReleases {
-        release_id: String::from("10.6"),
-        release_name: String::from("MariaDB Server 10.6"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Long Term Support"),
-        release_eol_date: Some(parse_from_year_month_date("2026-07-06").unwrap()),
-    };
-    releases[8] = mariadb_vm::MajorReleases {
-        release_id: String::from("10.5"),
-        release_name: String::from("MariaDB Server 10.5"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Long Term Support"),
-        release_eol_date: Some(parse_from_year_month_date("2025-06-24").unwrap()),
-    };
-    releases[9] = mariadb_vm::MajorReleases {
-        release_id: String::from("10.4"),
-        release_name: String::from("MariaDB Server 10.4"),
-        release_status: String::from("Stable"),
-        release_support_type: String::from("Long Term Support"),
-        release_eol_date: Some(parse_from_year_month_date("2024-06-18").unwrap()),
-    };
-    releases
-}
-
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = {
@@ -124,55 +25,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     };
     println!("args.root: {:?}\n", args.root);
 
-    let releases = match &args.command {
-        Commands::Ls {}// | Commands::Env {}
-        => Vec::with_capacity(0),
-        _ => {
-            match std::env::var("SKIP_REMOTE_VERSION_REFRESH") {
-                Ok(_) => offline_major_releases(),
-                Err(_) => {
-                    mariadb_vm::versions_from_remote().await?
-                }
-            }
-        }
+    let releases: Vec<mariadb_vm::MajorReleases> = match &args.command {
+        Commands::Ls {} | Commands::Env {} => Vec::with_capacity(0),
+        _ => match std::env::var("SKIP_REMOTE_VERSION_REFRESH") {
+            Ok(_) => mariadb_vm::offline_major_releases(),
+            Err(_) => mariadb_vm::versions_from_remote().await?,
+        },
     };
 
     match &args.command {
         Commands::Download { version } => {
-            let ver: String = resolve_version(
+            let release_id: String = mariadb_vm::resolve_version(
                 match version {
                     Some(v) => v,
                     None => "latest",
                 },
-                releases,
+                &releases,
             );
-            match mariadb_vm::download(&ver, &args.vm_root, false).await? {
+            match mariadb_vm::download(&release_id, &args.vm_root, false).await? {
                 Some(filepath) => println!("Downloaded: {:?}", filepath),
                 None => {}
             }
         }
         Commands::Ls {} => command::default_ls_command(&args)?,
         Commands::LsRemote {} => {
-            let mut release_ids = releases
-                .iter()
-                .map(|rel| rel.release_id.clone())
-                .collect::<Vec<String>>();
-            release_ids.sort();
-            for release_id in release_ids.iter() {
-                println!("{}", release_id)
+            for release in releases {
+                println!("{}", release.release_id)
             }
         }
         _ => command::default_command(&args)?,
     }
     config::maybe_config_file_write(&args)?;
     Ok(())
-}
-
-fn resolve_version(version: &str, aot_versions: Vec<mariadb_vm::MajorReleases>) -> String {
-    // Future work:
-    // Semver range queries
-    match version {
-        "latest" => aot_versions[aot_versions.len() - 1].release_id.clone(),
-        s => s.to_string(),
-    }
 }
--- src/mariadb_vm.rs
+++ src/mariadb_vm.rs
@@ -1,4 +1,5 @@
 extern crate reqwest;
+
 use crate::errors::MariaDbVmError;
 
 pub async fn download(
@@ -8,40 +9,51 @@ pub async fn download(
 ) -> Result<Option<std::ffi::OsString>, MariaDbVmError> {
     // TODO: mirrors, checksum
     let (url, filename) = {
-        let _parts = if cfg!(windows) {
-            (
-                "https://archive.mariadb.org//mariadb-",
-                version,
-                "/winx64-packages/",
-                "mariadb-",
-                version,
-                "-winx64.zip",
-            )
-        } else if cfg!(linux) {
-            (
-                "https://archive.mariadb.org/mariadb-",
-                version,
-                "/bintar-linux-systemd-x86_64/",
-                "mariadb-",
-                version,
-                "-preview-linux-systemd-x86_64.tar.gz",
-            )
-        } else {
-            (
-                "https://archive.mariadb.org/mariadb-",
-                version,
-                "/bintar-freebsd130-x86_64/",
-                "mariadb-",
-                version,
-                "-freebsd13.0-x86_64.tar.gz",
-            )
+        let _parts = match std::env::consts::OS {
+            "linux" => (
+                format!(
+                    "https://downloads.mariadb.org/rest-api/mariadb/{version}/",
+                    version = version
+                ),
+                format!(
+                    "mariadb-{version}-linux-systemd-{arch}.tar.gz",
+                    version = version,
+                    arch = std::env::consts::ARCH
+                ),
+            ),
+            "freebsd" => (
+                format!(
+                    "https://archive.mariadb.org/mariadb-{version}/bintar-freebsd130-{arch}/",
+                    version = version,
+                    arch = std::env::consts::ARCH
+                ),
+                format!(
+                    "mariadb-{version}-freebsd13.0-{arch}.tar.gz",
+                    version = version,
+                    arch = std::env::consts::ARCH
+                ),
+            ),
+            "windows" => (
+                format!(
+                    "http://downloads.mariadb.org/rest-api/mariadb/{version}/",
+                    version = version
+                ),
+                format!(
+                    "mariadb-{version}-win{arch}.zip",
+                    version = version,
+                    arch = match std::env::consts::ARCH {
+                        "x86" => "x32",
+                        "x86_64" => "x64",
+                        a => a,
+                    }
+                ),
+            ),
+            _ => unimplemented!("Build from source"),
         };
+
         (
-            reqwest::Url::parse(&format!(
-                "{}{}{}{}{}{}",
-                _parts.0, _parts.1, _parts.2, _parts.3, _parts.4, _parts.5
-            ))?,
-            format!("{}{}{}", _parts.3, _parts.4, _parts.5),
+            reqwest::Url::parse(&format!("{}{}", _parts.0, _parts.1))?,
+            _parts.1,
         )
     };
     let target_dir = std::path::Path::new(target_dir.as_os_str())
@@ -53,8 +65,8 @@ pub async fn download(
     } else if !target_dir.is_dir() {
         std::fs::create_dir_all(target_dir)?;
     }
-    let body = reqwest::get(url).await?.bytes().await?;
-    std::fs::write(&target_file, body)?;
+    let response = reqwest::get(url).await?;
+    std::fs::write(&target_file, response.bytes().await?)?;
     Ok(Some(std::ffi::OsString::from(target_file)))
 }
 
@@ -96,8 +108,7 @@ struct ListOfMajorAndMinorReleases {
     major_releases: Vec<MajorReleases>,
 }
 
-#[derive(serde::Deserialize, Debug)]
-// #[allow(dead_code)]
+#[derive(Clone, serde::Deserialize, Debug)]
 pub struct MajorReleases {
     pub release_id: String,
     pub release_name: String,
@@ -109,8 +120,157 @@ pub struct MajorReleases {
     pub release_eol_date: Option<chrono::NaiveDate>,
 }
 
+/*
+# To support old versions for testing purposes which are not on dlm.mariadb.com
+case ${mariadb_server_version} in
+    *10.1[0-9]*) ;; # everything >= 10.10 is on the new dlm for sure
+    *10.0*) ;& # 10.0 releases are on the old download server only
+    *10.1*) ;& # same for 10.1
+    *10.2*) ;& # same for 10.2
+    *10.3.[0-2]*|*10.3.[0-9]) ;& # 10.3: old server has <= .32, dlm has >= .29, we switch at .30
+    *10.4.[0-1]*|*10.4.[0-9]) ;& # 10.4: dlm has >= 10.4.20
+    *10.5.[0-9])     # 10.5: dlm has >= 10.5.10
+    url_base="downloads.mariadb.com"
+        url_mariadb_repo="https://${url_base}/MariaDB"
+    mariadb_server_version_real=$mariadb_server_version
+    ;;
+esac
+*/
+
+const AOT_MARIADB_VERSIONS_LEN: usize = 10;
+
+fn parse_from_year_month_date(date_str: &str) -> chrono::ParseResult<chrono::NaiveDate> {
+    chrono::NaiveDate::parse_from_str(date_str, "%Y-%m-%d")
+}
+
+// From https://dlm.mariadb.com/rest/releases/mariadb_server/ 2024-04-06
+pub fn offline_major_releases() -> Vec<MajorReleases> {
+    let mut releases: Vec<MajorReleases> = Vec::with_capacity(AOT_MARIADB_VERSIONS_LEN);
+
+    releases[0] = MajorReleases {
+        release_id: String::from("10.4"),
+        release_name: String::from("MariaDB Server 10.4"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Long Term Support"),
+        release_eol_date: Some(parse_from_year_month_date("2024-06-18").unwrap()),
+    };
+    releases[1] = MajorReleases {
+        release_id: String::from("10.5"),
+        release_name: String::from("MariaDB Server 10.5"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Long Term Support"),
+        release_eol_date: Some(parse_from_year_month_date("2025-06-24").unwrap()),
+    };
+    releases[2] = MajorReleases {
+        release_id: String::from("10.6"),
+        release_name: String::from("MariaDB Server 10.6"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Long Term Support"),
+        release_eol_date: Some(parse_from_year_month_date("2026-07-06").unwrap()),
+    };
+    releases[3] = MajorReleases {
+        release_id: String::from("10.11"),
+        release_name: String::from("MariaDB Server 10.11"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Long Term Support"),
+        release_eol_date: Some(parse_from_year_month_date("2028-02-16").unwrap()),
+    };
+    releases[4] = MajorReleases {
+        release_id: String::from("11.0"),
+        release_name: String::from("MariaDB Server 11.0"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Short Term Support"),
+        release_eol_date: Some(parse_from_year_month_date("2024-06-06").unwrap()),
+    };
+    releases[5] = MajorReleases {
+        release_id: String::from("11.1"),
+        release_name: String::from("MariaDB Server 11.1"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Short Term Support"),
+        release_eol_date: Some(parse_from_year_month_date("2024-08-21").unwrap()),
+    };
+    releases[6] = MajorReleases {
+        release_id: String::from("11.2"),
+        release_name: String::from("MariaDB Server 11.2"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Short Term Support"),
+        release_eol_date: Some(parse_from_year_month_date("2024-11-21").unwrap()),
+    };
+    releases[7] = MajorReleases {
+        release_id: String::from("11.3"),
+        release_name: String::from("MariaDB Server 11.3"),
+        release_status: String::from("Stable"),
+        release_support_type: String::from("Short Term Support"),
+        release_eol_date: None,
+    };
+    releases[8] = MajorReleases {
+        release_id: String::from("11.4"),
+        release_name: String::from("MariaDB Server 11.4"),
+        release_status: String::from("RC"),
+        release_support_type: String::from("Long Term Support"),
+        release_eol_date: None,
+    };
+    releases[9] = MajorReleases {
+        release_id: String::from("11.5"),
+        release_name: String::from("MariaDB Server 11.5"),
+        release_status: String::from("Alpha"),
+        release_support_type: String::from("Short Term Support"),
+        release_eol_date: None,
+    };
+
+    releases
+}
+
 pub async fn versions_from_remote() -> Result<Vec<MajorReleases>, Box<dyn std::error::Error>> {
     let response = reqwest::get("https://downloads.mariadb.org/rest-api/mariadb/").await?;
-    let list_of_major_and_minor_releases: ListOfMajorAndMinorReleases = response.json().await?;
+    let mut list_of_major_and_minor_releases: ListOfMajorAndMinorReleases = response.json().await?;
+    list_of_major_and_minor_releases
+        .major_releases
+        .sort_by(|rel0, rel1| rel0.release_id.cmp(&rel1.release_id));
     Ok(list_of_major_and_minor_releases.major_releases)
+}
+
+/// Given a version string resolve various version names to their numerical meanings
+pub fn resolve_version(version: &str, releases: &Vec<MajorReleases>) -> String {
+    // Future work:
+    // - Semver range queries
+    match version {
+        "alpha" => {
+            releases
+                .iter()
+                .rev()
+                .find(|rel| rel.release_status == "Alpha")
+                .cloned()
+                .unwrap()
+                .release_id
+        }
+        "latest" => {
+            releases
+                .iter()
+                .rev()
+                .find(|rel| rel.release_status == "Stable")
+                .cloned()
+                .unwrap()
+                .release_id
+        }
+        "LTS" | "lts" => {
+            releases
+                .iter()
+                .rev()
+                .find(|rel| rel.release_support_type == "Long Term Support")
+                .cloned()
+                .unwrap()
+                .release_id
+        }
+        "RC" | "rc" => {
+            releases
+                .iter()
+                .filter(|rel| rel.release_status == "RC")
+                .cloned()
+                .next()
+                .unwrap()
+                .release_id
+        }
+        s => s.to_string(),
+    }
 }
